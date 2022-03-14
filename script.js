5cf0085c43e1667a67ffba9a95096eb59069334b
DDubrovinas <kaipvisada@gmail.com>
2022-03-15 01:49:23 +0200

    Atsiskaitymas

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-alert("Veikia");
+"use strict";
+
+var buttons = document.getElementsByTagName("button");
+
+for (var x of buttons){
+    x.addEventListener("click", function(){
+        for (var y of buttons){
+            y.classList.remove("active");
+        }
+
+        this.classList.add("active");
+        var index = this.getAttribute("index");
+        swiper.slideTo(index, 1000);
+    })
+}
+
+
+swiper.on("slideChange", function(){
+    var activeButton = button[swiper.activeIndex];
+    for(var x of buttons){
+        x.classList.remove("active");
+    }
+    activeButton.classList.add("active");
+    console.log(activeButton);
+});
+
+
